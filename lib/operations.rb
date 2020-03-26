e8e7c78d315996f18a8621bdef5a4c674b9c7976
Derek phung <dragevards@gmail.com>
2020-03-26 18:28:31 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,13 @@
 def unsafe?(speed)
-  
+  if speed > 60 || speed < 40
+    true
+  else
+    false
 end
 
 
 def not_safe?(speed)
-	if speed < 60 && speed > 40? true : false
+	if speed > 60 || speed < 40? true : false
 end
 	
 
